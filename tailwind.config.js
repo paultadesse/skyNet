2d80e2fb5fb95edc025c83bad4178a2bbe82ef59
Paulos Tadesse <paulosta952@gmail.com>
2021-11-04 20:30:06 +0300

    :rocket: dark mode and light mode applied

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
     purge: [
       './resources/**/*.vue'
     ],
-    darkMode: false, // or 'media' or 'class'
+    darkMode: 'class', // or 'media' or 'class'
     theme: {
         extend: {
           animation:{
