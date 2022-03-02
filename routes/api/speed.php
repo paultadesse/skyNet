f13544510d753f9ff25f1e64b826d7c79e12336c
Paulos Tadesse <paulosta952@gmail.com>
2022-03-02 18:14:59 +0300

    :rocket: user need to be authenticated to create speed

--- routes/api/speed.php
+++ routes/api/speed.php
@@ -3,5 +3,7 @@
 use App\Http\Controllers\SpeedController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/speed/create', [SpeedController::class, 'store']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+	Route::post('/speed/create', [SpeedController::class, 'store']);
+});
 Route::get('/speed', [SpeedController::class, 'index']);
