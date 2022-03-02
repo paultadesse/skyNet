97598062bcc71c2d09d5077e213fb091f957d45d
Paulos Tadesse <paulosta952@gmail.com>
2022-03-02 18:22:54 +0300

    :rocket: user need to be authenticated to create site location

--- routes/api/site-location.php
+++ routes/api/site-location.php
@@ -3,5 +3,7 @@
 use App\Http\Controllers\SiteLocationController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/site-location/create', [SiteLocationController::class, 'store']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+	Route::post('/site-location/create', [SiteLocationController::class, 'store']);
+});
 Route::get('/site-locations', [SiteLocationController::class, 'index']);
