999c9e1647cbad77030e0ecb3ed6faabef4fbc27
Paulos Tadesse <paulosta952@gmail.com>
2022-03-07 16:35:29 +0300

    :zap:

--- laravel/app/Http/Resources/SiteLocationResource.php
+++ laravel/app/Http/Resources/SiteLocationResource.php
@@ -20,6 +20,5 @@ class SiteLocationResource extends JsonResource
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
-        // return parent::toArray($request);
     }
 }
